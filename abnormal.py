86812903039b011d6141d9373c2a4c9a3d18e6b7
mango941231 <41893110+mango941231@users.noreply.github.com>
2018-10-26 10:12:02 +0800

    Update abnormal.py

--- abnormal.py
+++ abnormal.py
@@ -7,12 +7,12 @@ gpus = int(sys.argv[1])
 
 def sendemail():
     sender = 'he1119197389@163.com'
-    password = 'he941231'
+    password = 'he941231'  #密码是邮箱界面‘设置’里的客户端授权密码
     host = 'smtp.163.com'
     receivers = ['15634218103@163.com']
     message = MIMEText('爬虫挂掉~', 'plain', 'utf-8')
     message['From'] = 'he1119197389@163.com'
-    message['To'] = '15634218103@163.com'
+    message['To'] = '15634218103@163.com'        #From、TO必须和前面的sender、receivers保持一致
     message['Subject'] = Header('我是标题~', 'utf-8')
     try:
         smtpObj = smtplib.SMTP_SSL(host, 465)
