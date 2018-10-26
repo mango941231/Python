061657cf66136952d65b57aefadbb92554d8848d
mango941231 <41893110+mango941231@users.noreply.github.com>
2018-10-26 09:49:44 +0800

    Add files via upload

---
+++ abnormal.py
@@ -0,0 +1,57 @@
+import psutil
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import sys
+gpus = int(sys.argv[1])
+
+def sendemail():
+    sender = 'he1119197389@163.com'
+    password = 'he941231'
+    host = 'smtp.163.com'
+    receivers = ['15634218103@163.com']
+    message = MIMEText('爬虫挂掉~', 'plain', 'utf-8')
+    message['From'] = 'he1119197389@163.com'
+    message['To'] = '15634218103@163.com'
+    message['Subject'] = Header('我是标题~', 'utf-8')
+    try:
+        smtpObj = smtplib.SMTP_SSL(host, 465)
+        smtpObj.login(sender, password)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("发送邮件成功")
+    except smtplib.SMTPException as e:
+        print("发送邮件产生错误")
+        print(e)
+        smtpObj.close()
+
+
+def abnor(g):
+    while 1:
+        pids = psutil.pids()
+        if g not in pids:
+            break
+    sendemail()
+
+
+
+if __name__ == '__main__':
+    abnor(gpus)
+
+
+
+
+# for pid in pids:
+#     p = psutil.Process(pid)
+#     #print('%s      %s' %(pid,p.name()))
+#     if pid == 1100:
+#         pass
+#     else:
+#         try:
+#             smtpObj = smtplib.SMTP_SSL(host,465)
+#             smtpObj.login(sender,password)
+#             smtpObj.sendmail(sender,receivers,message.as_string())
+#             print("发送邮件成功")
+#         except smtplib.SMTPException as e:
+#             print("发送邮件产生错误")
+#             print(e)
+#         smtpObj.close()
